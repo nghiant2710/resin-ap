bb23286625341699c0eac4257d9386ec6a176a8a
James <james@resin.io>
2015-01-16 16:51:12 +0000

    fix typo mistake

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@
 ifdown wlan0
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE \
 	&& iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT \
-	&& iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT \
+	&& iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT
 
 # Restart DNSMASQ service
 ifup wlan0
